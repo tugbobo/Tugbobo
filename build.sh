05d31ab3abdc23ac11269bb4c280095cf3258847
Dami Tugbobo <dami@tugbobo.com>
2025-02-18 00:07:35 -0500

    :construction_worker: Add TailwindCli to CI build

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+# Install .NET
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 8.0 -InstallDir ./dotnet
 ./dotnet/dotnet --version
+
+# Download TailwindCSS binary
+curl -L "https://github.com/tailwindlabs/tailwindcss/releases/latest/download/tailwindcss-linux-x64" -o Tailwind/tailwindcss-linux-x64
+chmod +x Tailwind/tailwindcss-linux-x64
+
+# Run .NET publish
 ./dotnet/dotnet publish -c Release -o output
